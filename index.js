2081785a76af13ad66084d06bd535e72bbe4478c
mauri <morocobt@gmail.com>
2023-10-26 10:28:19 -0400

    get key asynchronously

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 import express from 'express'
 import axios from 'axios'
-import { readFile } from 'node:fs';
+import { readFile } from 'node:fs/promises';
 
 const app = express()
 const port = 3000
 
 app.use(express.urlencoded())
 
-let key = ''
-
-readFile('key.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  key = JSON.parse(data).key;
-});
-
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
 app.post('/', async (req, res) => {
-    const querry = req.body.search
+    const endPoint = 'https://youtube.googleapis.com/youtube/v3/search'
+    const parameters = {
+        params: {
+            part: 'snippet',
+            maxResults: '25',
+            key: await getKey(),
+            q: req.body.search
+        }
+    }
 
     try {
-        const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${querry}&key=${key}`)
-        console.log(response.data.items[0].snippet.thumbnails.high)
+        const response = await axios.get(endPoint, parameters)
+        console.log(response.data.items[0].snippet)
         res.render('index.ejs', {response: response})
     } catch(error) {
         console.error(error)
-        res.render('index.ejs', {title: 'error'})
+        res.render('index.ejs', {error: error})
     }
 })
+
+async function getKey() {
+    try {
+        const filePath = new URL('./key.json', import.meta.url);
+        const contents = await readFile(filePath, { encoding: 'utf8' });
+        return JSON.parse(contents).key;
+      } catch (err) {
+        console.error(err.message);
+      }
+}
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
